d585e76a21d5a3a4f2d59ca53613227e8cbf917c
Steven Diamond <stevend2@stanford.edu>
2013-07-05 14:28:50 -0700

    preparing for deployment

--- dcp_site/settings.py
+++ dcp_site/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-#STATIC_ROOT = '/afs/ir/group/dcp/WWW/'
+STATIC_ROOT = '/var/www/dcp.stanford.edu/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
