0735ba03c5648577bbabad952dee9bd214f28de1
Steven Diamond <stevend2@stanford.edu>
2013-07-06 12:08:04 -0700

    url error

--- dcp_sandbox/urls.py
+++ dcp_sandbox/urls.py
@@ -5,6 +5,5 @@ from dcp_sandbox import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^parse$', views.parse, name='parse'),
-    url(r'^solveLP$', views.solveLP, name='solveLP'),
     url(r'^test$', views.test, name='test'),
 )
