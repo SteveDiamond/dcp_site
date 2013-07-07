d02433a40ebdee87818f3de7e95759f01e8ce368
Steven Diamond <stevend2@stanford.edu>
2013-07-06 22:27:53 -0700

    added list of functions

--- dcp_sandbox/static/dcp_sandbox/js/sandbox.js
+++ dcp_sandbox/static/dcp_sandbox/js/sandbox.js
@@ -9,6 +9,15 @@
             TreeConstructor.helpActive = !TreeConstructor.helpActive;
             TreeConstructor.processParseTree(TreeConstructor.root); 
         });
+        // Enable function selection.
+        $(window).bind('hashchange', function(e) {
+            var atom = window.location.hash;
+            var hashIndex = atom.indexOf("#");
+            if (hashIndex != -1 && atom.length > 1) {
+                window.location.hash = "";
+                TreeConstructor.parseObjective( atom.substr(hashIndex + 1) );
+            }
+        });
         // Show prompt.
         var promptTree = {
                          name: TreeConstants.PROMPT,
