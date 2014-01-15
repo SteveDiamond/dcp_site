2674d02a254b828072445200b93aeac1a1d77594
Steven Diamond <stevend2@stanford.edu>
2014-01-14 21:17:50 -0800

    added constants info to prompt

--- dcp_sandbox/static/dcp_sandbox/js/TreeConstants.js
+++ dcp_sandbox/static/dcp_sandbox/js/TreeConstants.js
@@ -55,6 +55,7 @@ TreeConstants.LEAVES = {
 TreeConstants.PROMPT_LEAF_LEGEND = [
     "Available Variables: x,y,z; u,v,w [positive]",
     "Available Parameters: a,b,c; d,e,f [positive]",
+    "Available Constants: Any",
     "Available Functions: See Functions tab",
 ]
 // Editable and fixed text classes.
